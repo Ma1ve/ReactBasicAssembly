845a25f56f6f7e694dafa0eceab8f165554f677b
Ma1ve <93385505+Ma1ve@users.noreply.github.com>
2023-02-14 02:31:23 +0300

    add BundleAnalyzerPlugin babel-runtime

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.tsx'),
@@ -39,6 +40,9 @@ module.exports = {
   plugins: [
     new HTMLWebpackPlugin({
       template: path.resolve(__dirname, '..', './src/index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [{ from: './src/source', to: 'dest' }],
     }),
   ],
 };
