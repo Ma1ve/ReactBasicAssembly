f6a0d0fbfe7dab8a2afe80fce8e4a5f4922ffdb8
Ma1ve <93385505+Ma1ve@users.noreply.github.com>
2023-02-13 16:57:39 +0300

    add styles image logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import './index.css';
+
+import IMAGE from './react.png';
+import LOGO from './logo.svg';
+
 export const App = () => {
   return (
-    <>
-      <div>React TypeScript Webpack Starter Template</div>
-    </>
+    <div>
+      <h1>React TypeScript Webpack Starter Template</h1>
+      <img src={IMAGE} alt="React logo" height="300" />
+      <img src={LOGO} alt="React logo" height="300" />
+    </div>
   );
 };
