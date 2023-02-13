ef74501df6ba2bec5f240cce8d550dd0f349bd2e
Ma1ve <93385505+Ma1ve@users.noreply.github.com>
2023-02-13 19:06:31 +0300

    setting webpack for multiple environments

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ import LOGO from './logo.svg';
 export const App = () => {
   return (
     <div>
-      <h1>React TypeScript Webpack Starter Template</h1>
+      <h1>
+        React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}
+      </h1>
       <img src={IMAGE} alt="React logo" height="300" />
       <img src={LOGO} alt="React logo" height="300" />
     </div>
