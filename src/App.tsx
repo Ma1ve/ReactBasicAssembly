dfad6ac9eba1e99a853c6a2b2668342bbfb06ef6
Ma1ve <93385505+Ma1ve@users.noreply.github.com>
2023-02-13 19:33:49 +0300

    add hot reloading

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './index.css';
 
 import IMAGE from './react.png';
 import LOGO from './logo.svg';
+import { ClickCounter } from './ClickCounter';
 
 export const App = () => {
   return (
@@ -11,6 +12,7 @@ export const App = () => {
       </h1>
       <img src={IMAGE} alt="React logo" height="300" />
       <img src={LOGO} alt="React logo" height="300" />
+      <ClickCounter />
     </div>
   );
 };
